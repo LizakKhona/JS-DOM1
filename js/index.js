131aed38478901000e65c52c292e8a79d88c9b1d
LizakKhona <liz.honahbeeva2009@gmail.com>
2025-11-15 21:10:18 +0100

    dom1

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const inputElem = document.getElementById("input-js");
+const buttonElem = document.getElementById("button-js");
+
+inputElem.addEventListener("input", onChangeBtnContent)
+function onChangeBtnContent(event) {
+    buttonElem.textContent = event.target.value
+}
+
+
+
+const imgElem = document.querySelector(".gallery-img-js");
+imgElem.src = "/images/evening-city.jpg"
+imgElem.alt = "An evening city"
+
+
+
+const linkElem = document.querySelector(".box-link");
+const picElem = document.querySelector(".box-img");
+linkElem.href = "https://www.instagram.com/accounts/login/";
+picElem.alt = "a photo of a sunset"
+
+
+
+const listElem = document.querySelector(".list");
+listElem.firstElementChild.textContent = "A new sentence"
